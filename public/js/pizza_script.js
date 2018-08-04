ca5fbe68238f23a4b4da91dd2898d8ec8aba076a
Josh Varney <joshcainvarney@gmail.com>
2018-08-04 15:56:12 -0400

    cant get require when active to work so trying to finish everything exept that first

--- public/js/pizza_script.js
+++ public/js/pizza_script.js
@@ -24,3 +24,31 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+var FormStuff = {
+  
+  init: function() {
+    this.applyConditionalRequired();
+    this.bindUIActions();
+  },
+  
+  bindUIActions: function() {
+    $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
+  },
+  
+  applyConditionalRequired: function() {
+    
+    $(".require-if-active").each(function() {
+      var el = $(this);
+      if ($(el.data("require-pair")).is(":checked")) {
+        el.prop("required", true);
+      } else {
+        el.prop("required", false);
+      }
+    });
+    
+  }
+  
+};
+
+FormStuff.init();
